476d361caaad6f1217b3cc40dd00946f0cb60431
Nafis Rahman <nmr278@nyu.edu>
2021-07-21 22:22:20 -0400

    Added questions and questionBank variables

---
+++ assets/js/script.js
@@ -0,0 +1,29 @@
+/*=================================
+ DEPENDENCIES 
+ ==================================*/
+
+ /*=================================
+ DATA
+ ==================================*/
+
+ /*=================================
+ FUNCTIONS
+ ==================================*/
+
+ /*=================================
+ INITIALIZATION 
+ ==================================*/
+
+ button has data attribute with each answer to the question 
+ question button counter 
+
+question data attribute
+
+ choices in 
+
+ question1 = {choices : ["monkey", "horse", "pizza", "computer"],
+         correctAnswer: "monkey",
+         question: "Which is the closest to a hominid?"
+ }
+
+ questions = [question1, question2, question3, question4,]
