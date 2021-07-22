fa37782f478af230dd52202869b0bd7f4cd682da
Nafis Rahman <nmr278@nyu.edu>
2021-07-21 22:23:10 -0400

    Added questions and questionBank variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,28 +2,78 @@
  DEPENDENCIES 
  ==================================*/
 
- /*=================================
- DATA
- ==================================*/
+/* Event listeners:
+ - Start Quiz button
+ - High scores link
+ - 4 Buttons
+ - Enter initials submit button
+ - Go back button
+ - 
 
- /*=================================
- FUNCTIONS
- ==================================*/
+*/
 
- /*=================================
- INITIALIZATION 
- ==================================*/
+/*=================================
+DATA
+==================================*/
 
- button has data attribute with each answer to the question 
- question button counter 
+var titles = {
+        startGame : 'Coding Quiz Challenge',
+        endGame : 'All done!',
+        highScores : 'High Scores'
+};
+
+var question1 = {
+        question : 'What does JSON stand for?',
+        choice : ['Javascript Onset','Javascript Object Nouns','Javascript Object Notation', 'Jason'],
+        answer : 'Javascript Object Notation'
+};
+
+var question2 = {
+        question : 'How many primitive data types are there in Javascript?',
+        choice : ['1', '2', '3', '4'],
+        answer : '3'
+};
+
+var question3 = {
+        question : 'What datatype in Javascript is used to store numbers?',
+        choice : ['int', 'bool', 'number', 'float'],
+        answer : 'number'
+};
+
+var question4 = {
+        question : 'You declare a variable in Javascript using which keyword?',
+        choice : ['declare', 'var', 'in', 'new'],
+        answer : 'var'
+};
+
+var question5 = {
+        question : 'What is a shortcut notation to increment the variable "count" by 1?',
+        choice : ['count++', 'count++=', 'count*', 'count+1'],
+        answer : 'count++'
+};
+
+var questionBank = [
+        question1,
+        question2,
+        question3,
+        question4,
+        question5
+];
+
+/*=================================
+FUNCTIONS
+==================================*/
+
+/*=================================
+INITIALIZATION 
+==================================*/
+
+button has data attribute with each answer to the question 
+question button counter 
 
 question data attribute
 
  choices in 
 
- question1 = {choices : ["monkey", "horse", "pizza", "computer"],
-         correctAnswer: "monkey",
-         question: "Which is the closest to a hominid?"
- }
 
- questions = [question1, question2, question3, question4,]
++
